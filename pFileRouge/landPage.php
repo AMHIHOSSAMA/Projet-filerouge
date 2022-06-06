80e3c82f16bbf4292034e84907518e016eb34d90
AMHIHOSSAMA <92022179+AMHIHOSSAMA@users.noreply.github.com>
2022-06-06 16:57:28 +0100

    Add files via upload

---
+++ pFileRouge/landPage.php
@@ -0,0 +1,61 @@
+<?php
+
+
+
+include('header.php');
+
+?>
+
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img id="logo" src="images/logo.png" alt="logo flance "></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Projets</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">À propos</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+      <button type="button" id="btn1" class="btn btn-outline">Login</button>
+      <button type="button" id="btn2" class="btn">Sign up</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- the main of landing page -->
+<!-- 001 -->
+
+<main>
+<div class="container">
+  <div class="row">
+    <div id="" class="col">
+      <h1 id="bigtitl" class="content">Engagez les meilleurs freelances pour n'importe quel travail, en ligne.</h1>
+      <P id="paragr1" class="content">Une section de grands Freelancers sont prêts à travailler pour vous, faites votre travail dans une variété de catégories</P>
+      <div class="row">
+        <div class="col">
+        <button type="button" id="btn3" class="btn">Rejoignez-nous</button>
+        </div>
+        <div class="col">
+        <img id="flower" src="images/flower.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img id="heroimage" src="images/imagecouver.png" alt="">
+    </div>
+  </div>
+</div>
+</main>
